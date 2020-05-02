7abc7cc7b2b8fb14e52f3ef093f03f9c1f2a0cd7
soroush <soroush_safarii@yahoo.com>
2020-05-02 18:56:37 +0430

    Initial Commit

---
+++ hello/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by soroush safari on 5/2/20.
+//  Copyright © 2020 soroush safari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
